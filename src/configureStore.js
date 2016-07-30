5038312bc284daeca34e47ae35567c90a80c4a3a
davidhu34 <b01901133@gmail.com>
2016-07-30 16:29:13 +0800

    redux- promise/logger

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,49 +1,29 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import promise from 'redux-promise'
+import createLogger from 'redux-logger'
 import throttle from 'lodash/throttle'
+
 import { loadState, saveState } from './localStorage'
 import chatApp from './reducers';
 
-const supportPromise = ( store ) => ( next ) =>
-    ( action ) =>
-        ( typeof action.then === 'function' )? (
-            action.then( next )
-        ) : next( action )
-
-const logger = ( store ) => ( next ) =>
-    ( !console.group )? (
-        next
-    ) : ( ( action ) => {
-        console.group( action.type )
-        console.log( '%c prev state', 'color:grey', store.getState() )
-        console.log( '%c action', 'color:blue', action )
-        const returnValue = next( action )
-        console.log( '%c next state', 'color:green', store.getState() )
-        console.groupEnd( action.type )
-        return returnValue
-    })
-
-const wrapDispatchMiddleware = ( middlewares, store ) => {
-    middlewares.slice().reverse().forEach( middleware =>
-        store.dipatch = middleware( store )( store.dispatch )
-    )
-}
-
-
 const freshStore = () => {
-    const store = createStore( chatApp )
-    const middlewares = [ supportPromise, logger ]
-    wrapDispatchMiddleware( middlewares, store )
-    return store
+    const middlewares = [ promise ]
+    middlewares.push( createLogger() )
+    return createStore(
+        chatApp,
+        applyMiddleware( ...middlewares )
+    )
 }
 
 const localStorageStore = () => {
+    const middlewares = [ promise ]
+    middlewares.push( createLogger() )
     const persistedState = loadState()
     const lss = createStore(
         chatApp,
-        persistedState
+        persistedState,
+        applyMiddleware( ...middlewares )
     )
-    lss.dispatch = logDispatch( lss )
-    lss.dispatch = supportPromiseForDispatch( lss )
     lss.subscribe( throttle( () => {
         saveState( lss.getState() )
     }, 1000 ))
